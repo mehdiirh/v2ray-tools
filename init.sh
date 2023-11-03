b3819fcf8bcf14f9c68fe101846962c902138eb1
Mehdi Rahimi <mehdii.rh78@gmail.com>
2023-11-03 20:01:24 +0330

    [UPDATE] change nginx workers count with "sed"

--- init.sh
+++ init.sh
@@ -28,7 +28,8 @@ sed -i "s/%HOST%/${server}/g" "$server"
 ln -s /etc/nginx/sites-available/"$server" /etc/nginx/sites-enabled/
 
 systemctl edit --full nginx
-nano /etc/nginx/nginx.conf
+sed -i "s/worker_connections 768;/worker_connections 10000;/g"
+sed -i "s/# multi_accept on;/multi_accept on;/g"
 
 systemctl stop nginx
 sudo certbot certonly --standalone --preferred-challenges http --agree-tos -d "$server"
