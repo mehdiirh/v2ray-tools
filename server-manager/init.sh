921ae7641da01c0bb69a5df284c4e0ff6c7e57f7
Mehdi Rahimi <mehdii.rh78@gmail.com>
2024-05-30 03:15:22 +0330

    [UPDATE] move server-manager service instead of copy

--- server-manager/init.sh
+++ server-manager/init.sh
@@ -18,7 +18,7 @@ hexdump -vn16 -e'4/4 "%08X" 1 "\n"' /dev/urandom > token
 cp systemd.service server-manager.service
 sed -i "s|%WORKING_DIR%|$(pwd)|g" server-manager.service
 
-cp server-manager.service /etc/systemd/system/
+mv server-manager.service /etc/systemd/system/
 
 systemctl enable server-manager.service
 systemctl restart server-manager.service
